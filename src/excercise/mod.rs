9f61b8a643db226b4b09f43757e2feeaa1acad05
Rpetey317 <rubenandrelo31@gmail.com>
2024-04-24 18:11:06 -0300

    Added path checking

--- src/excercise/mod.rs
+++ src/excercise/mod.rs
@@ -4,13 +4,20 @@ pub struct Excercise {
     path: String,
 }
 
+#[allow(dead_code)] // Remove later
 impl Excercise {
-    fn new(assignature: String, path: String) -> Excercise {
-        Excercise {
+    fn new(assignature: String, path: String) -> Result<Excercise, std::io::Error> {
+        if !std::path::Path::new(&path).exists() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "File not found",
+            ));
+        }
+        Ok(Excercise {
             assignature,
             tags: Vec::new(),
             path,
-        }
+        })
     }
 
     fn add_tag(&mut self, tag: String) {
@@ -43,12 +50,14 @@ mod tests {
             "Álgebra Lineal".to_string(),
             "tests/testfiles/exc1.md".to_string(),
         )
+        .unwrap() //hardcoded test file, should always exist
     }
 
     #[test]
     fn test_01_excercise_belongs_to_assignature() {
         let exc = test_exc_1();
         assert_eq!(exc.is_for_assignature("Álgebra Lineal"), true);
+        assert_eq!(exc.is_for_assignature("Física 1"), false);
     }
 
     #[test]
@@ -64,6 +73,7 @@ mod tests {
         exc.add_tag("Matrices".to_string());
         exc.add_tag("Determinantes".to_string());
         assert_eq!(exc.has_tag("Matrices"), true);
+        assert_eq!(exc.has_tag("Determinantes"), true);
     }
 
     #[test]
@@ -79,5 +89,11 @@ mod tests {
     fn test_05_excercise_has_path() {
         let exc = test_exc_1();
         assert_eq!(exc.get_path(), "tests/testfiles/exc1.md");
+    }
+
+    #[test]
+    fn test_06_path_must_be_valid() {
+        let exc = Excercise::new("Álgebra Lineal".to_string(), "not/a/path.txt".to_string());
+        assert_eq!(exc.is_err(), true);
     }
 }
